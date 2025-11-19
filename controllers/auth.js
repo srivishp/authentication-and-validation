b39b7d4d966d713e2ced99a6c051ed571bca2406
Sri Vishnu Pasumarthi <srivishp@gmail.com>
2025-11-19 20:05:43 +0530

    Style flash messages

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,11 +3,17 @@ const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
     //* Accessing the key of the flash message
-    errorMessage: req.flash("error"),
+    errorMessage: message,
   });
 };
 
@@ -38,6 +44,7 @@ exports.postLogin = (req, res, next) => {
               res.redirect("/");
             });
           }
+          req.flash("error", "Invalid email or password.");
           res.redirect("/login");
         })
         .catch((err) => {
@@ -58,9 +65,16 @@ exports.postLogout = (req, res, next) => {
 
 // SIGN UP SECTION
 exports.getSignup = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
+    errorMessage: message,
   });
 };
 
@@ -71,6 +85,7 @@ exports.postSignup = (req, res, next) => {
 
   User.findOne({ email: email }).then((userDoc) => {
     if (userDoc) {
+      req.flash("error", "E-Mail exists already, please pick a different one.");
       return res.redirect("/signup");
     }
     //> Using bcrypt to hash a password with salt number 12
