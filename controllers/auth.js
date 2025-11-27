1943839459313de0c9a3ae737ccad165ca091002
Sri Vishnu <srivishp@gmail.com>
2025-11-27 16:14:31 +0530

    Implement token logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const crypto = require("crypto");
 const User = require("../models/user");
 const nodemailer = require("nodemailer");
 // const transporter = nodemailer.createTransport({
@@ -138,5 +139,40 @@ exports.getReset = (req, res, next) => {
     path: "/reset",
     pageTitle: "Reset Password",
     errorMessage: message,
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect("/reset");
+    }
+    const token = buffer.toString("hex");
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          req.flash("error", "No account found with this email.");
+          return res.redirect("/reset");
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then((result) => {
+        res.redirect("/");
+        transporter.sendMail({
+          to: req.body.email,
+          from: "srivishp@gmail.com",
+          subject: "Password reset",
+          html: `
+          <p>You requested a password reset</p>
+          <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+          `,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 };
--- models/user.js
+++ models/user.js
@@ -5,6 +5,8 @@ const userSchema = new Schema({
   // name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  resetToken: String,
+  resetTokenExpiration: Date,
   cart: {
     items: [
       {
